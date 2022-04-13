55271d31e90f400f020a0fee9ac6baf1e065770c
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2022-04-13 09:33:51 -0500

    Update executor.c

--- executor.c
+++ executor.c
@@ -1,6 +1,20 @@
 #include "monty.h"
 
-void executor(char *opcode)
+void executor(stack_t **stack, char *rdr, unsignedint lncnt)
 {
+	int x;
+	instruction_t function[] = VALIDOPS
+	
+	if (cmd == NULL)
+		return;
+	for (x = o; function[x].opcode; x++)
+	
+	if(strcmp(function[x].opcodes, rdr) == 0
+	   {
+		   funciton[x].f(stack,lncnt);
+		   return;
+	   }
+	   fprintf(stderr, "L%d: unknown instructions %s\n, lncn, rdr")
+	   arg = "error"
 	return;
 }
