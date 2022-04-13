90593c674eae77732b2eb53d267205aef7635328
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2022-04-12 19:36:01 -0500

    Update pall.c

--- pall.c
+++ pall.c
@@ -6,14 +6,16 @@
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
-
-	(void) line_number;
-	if (stack == NULL)
-		return;
-	while(current)
+	stack_t *current;
+		
+    if(stack)
+      {
+	 current = *stack;
+	 while(current)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+	     printf("%d\n", current->n);
+             current = current->next;
 	}
+      }
+return;
 }
