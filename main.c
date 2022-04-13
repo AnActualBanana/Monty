f05e6fcb7017c20f4d1e629976acbb8484339c8d
AnActualBanana <loganmcclain59@gmail.com>
2022-04-13 07:27:02 -0700

    main.c update

--- main.c
+++ main.c
@@ -1,49 +1,44 @@
 #include "monty.h"
 
-/** this program will read the lines of the monty file (provided by the user),
- * and run the appropriate functions based on the opcodes (push, pall, pint, pop, swap, add, nop) that they provide
- * @argc: number of arguments given (should be exactly 2, otherwise we give an error
- * @argv: the arguments given ([0] should be "monty", [1] should be the file we want to read. 
+/**
+ * this program will read the lines of the monty file (provided by the user),
+ * and run the appropriate functions based on the opcodes that they provide
+ * @argc: number of arguments given (should be exactly 2)
+ * @argv: the arguments given ([0] should be "monty", [1] should be the file.)
  * Return: success or error code
  */
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-	FILE *fd; /* the file we want to read */
+	FILE *fd = NULL;
 	char **buffer = malloc(1 * sizeof(char *));
 	ssize_t line_size = 0;
 	size_t *bufsize = (size_t *)1024;
 	char *opcode = malloc(5);
 	unsigned int count = 0;
 	int line_number = 0;
-	char *valid_opcodes[8];
-	if (argc != 2) /* check that correct number of arguments was given */
+	char *valid_opcodes[7];
+
+	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fd = fopen(argv[1], O_RDONLY);
 	if (fd == NULL)
 	{
-		printf("%s%s\n", "Error: Can't open file ", argv[1]);
+		fprintf(stderr, "%s%s\n", "Error: Can't open file ", argv[1]);
 		fclose(fd);
 		exit(EXIT_FAILURE);
 	}
-newline: while(line_size >= 0)
+	for (; line_size >= 0; line_number++)
 	{
-		line_number++;
 		line_size = getline(buffer, bufsize, fd);
 		opcode = strtok(*buffer, " ");
-		for (count = 0; count < 7; count++)
-		{
-			if (strcmp (opcode, valid_opcodes[count]) == 0)
-			{
-				(void)executor(opcode);
-				goto newline;
-			}
-		}
-		if (opcode == "nope")
-		   printf("L%d: unknown instruction %s\n", line_number, opcode);
-}
+		(void)executor(opcode);
+	}
+	fclose(fd);
+	free(buffer);
+	free(opcode);
 	return (0);
 }
