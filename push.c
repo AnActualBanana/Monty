358378d2c74176db14312383272a5d55512fe095
AnActualBanana <loganmcclain59@gmail.com>
2022-04-13 09:57:47 -0700

    push proto

--- push.c
+++ push.c
@@ -3,9 +3,20 @@
 /* this function will push an element to the stack, error cases for this function will be captured in the reader
  */
 
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number, char *p)
 {
-	(void)stack;
-	(void)line_number;
+	if (
+	stack_t *newnode = malloc (1 * sizeof(stack_t));
+	if (!newnode)
+	{
+		free(newnode);
+		fprintf(stderr, "Error: malloc failed");
+		arg = "error";
+		return;
+	}
+	(*stack)->prev = newnode;
+	newnode->next = (*stack);
+	newode->prev = NULL;
+	newnode->n = p;
 	return;
 }
